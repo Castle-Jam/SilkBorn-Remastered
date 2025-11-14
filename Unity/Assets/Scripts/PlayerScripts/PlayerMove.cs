24353f6967841cbdc38bcb9a8e8b08a4fecf7b1d
KonstiDerCharmeur <74175473+KonstiDerCharmeur@users.noreply.github.com>
2025-11-14 22:09:01 +0100

    Jump leck

--- Unity/Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ Unity/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -1,22 +1,57 @@
+using System;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] private InputActionReference jump;
     Vector2 movement = new Vector2();
-    
+    public Rigidbody2D rb;
+    bool isGrounded;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
 
+        jump.action.performed += OnJump;
+
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("We jump1.");
+        if (isGrounded)
+        {
+            isGrounded = false;
+            movement.y = context.ReadValue<float>();
+        }
+    }
 
     private void Update()
     {
-        Vector2 position =  transform.position;
-        position += movement * Time.deltaTime * speed;
-        transform.position =  position;
+        Vector2 position = transform.position;
+        position.x += movement.x * Time.deltaTime * speed;
+        position.y += movement.y * Time.deltaTime * jumpHeight;
+        transform.position = position;
+
     }
 
     public void GetPlayerMovement(InputAction.CallbackContext context)
     {
         movement.x = context.ReadValue<float>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            movement.y = 0;
+        }
+    }
+    public void GetPlayerDash(InputAction.CallbackContext context)
+    {
+        
     }
 }
