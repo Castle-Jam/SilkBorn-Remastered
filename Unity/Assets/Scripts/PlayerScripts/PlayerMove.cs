fbb2a3ef03c1ed5d4f61165498dfd146ab761719
Maximilian Irran <m.irran@commend.com>
2025-11-15 17:50:44 +0100

    implement move and gravity ray casting

--- Unity/Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ Unity/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -11,26 +11,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private InputActionReference jump;
     [SerializeField] private InputActionReference dash;
     [SerializeField] public float gravity;
-    [SerializeField] public float distanceMod;
     Vector2 movement = new Vector2();
-    public float floatHeight;     // Desired floating height.
-    public float liftForce;       // Force to apply when lifting the rigidbody.
-    public float damping;         // Force reduction proportional to speed (reduces bouncing).
     public Rigidbody2D rigidBody;
     bool isGrounded;
-    public Vector3 moveDirection;
     public float maxDashTime = 1.0f;
     [SerializeField] public float dashSpeed = 5.0f;
     public float dashStoppingSpeed = 0.1f;
     private float currentDashTime;
-
-    LayerMask layerMask;
+    private float width;
+    private float offset;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
 
-        layerMask = LayerMask.GetMask("Wall");
+        width = GetComponentInChildren<BoxCollider2D>().bounds.size.x;
+        offset = width / 2 * 1.1f;
 
         jump.action.performed += OnJump;
         dash.action.performed += OnDash;
@@ -51,10 +47,9 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        if (!isGrounded)
-        {
-            movement.y -= gravity;
-        }
+
+        movement.y -= gravity;
+
         Vector2 position = transform.position;
         float xMovement = movement.x * Time.deltaTime * speed;
         if (currentDashTime < maxDashTime)
@@ -62,23 +57,12 @@ public class PlayerMovement : MonoBehaviour
             xMovement *= dashSpeed;
             currentDashTime += dashStoppingSpeed;
         }
-
-        position.y += movement.y * Time.deltaTime * jumpHeight;
-        float width = GetComponentInChildren<BoxCollider2D>().bounds.size.x;
-        Vector3 pos = new Vector3(transform.position.x + width * 1.1f / 2, 0, 0);
-
-        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.right, 100);
-
-        if (hit)
-        {
-            Debug.Log("hit");
-            float distance = hit.point.x - transform.position.x - width / 2;
-            if (distance < xMovement)
-            {
-                xMovement = distance;
-            }
-        }
+        xMovement = RayCastX(position, xMovement);
         position.x += xMovement;
+
+        float yMovement = movement.y * Time.deltaTime * jumpHeight;
+        yMovement = RayCastY(position, yMovement);
+        position.y += yMovement;
 
         transform.position = position;
     }
@@ -87,22 +71,60 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = context.ReadValue<float>();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-            movement.y = 0;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
 
+    public float RayCastX(Vector2 position, float xMovement)
+    {
+        switch (xMovement)
+        {
+            case > 0.0f:
+                Vector3 pos = new Vector3(position.x + offset, position.y, 0);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.right, xMovement);
+
+                if (hit)
+                {
+                    float distance = hit.point.x - position.x - offset;
+                    if (distance < xMovement)
+                    {
+                        return distance;
+                    }
+                }
+                break;
+
+            case < 0.0f:
+                Vector3 pos1 = new Vector3(position.x - offset, position.y, 0);
+                RaycastHit2D hit1 = Physics2D.Raycast(pos1, Vector2.left, -xMovement);
+
+                if (hit1)
+                {
+                    float distance = position.x - hit1.point.x - offset;
+                    if (distance < -xMovement)
+                    {
+                        return -distance;
+                    }
+                }
+                break;
         }
+        return xMovement;
+    }
+
+    public float RayCastY(Vector2 position, float yMovement)
+    {
+        if (yMovement < 0)
+        {
+            Vector3 pos = new Vector3(position.x, position.y - offset, 0);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.down, -yMovement);
+
+            if (hit)
+            {
+                float distance = -hit.point.y - -position.y - offset;
+                if (distance < -yMovement)
+                {
+                    isGrounded = true;
+                    movement.y = 0;
+                    return -distance;
+                }
+            }
+        }
+        return yMovement;
     }
 }
-
-
