4300558446a22f5bfb38c70040c3e9dfef4eb123
Crafter1606 <167441275+Crafter1606@users.noreply.github.com>
2025-11-15 16:18:46 +0100

    Adding Health and Attack Tags and things

--- Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+<<<<<<< HEAD
     bool DamageAreaActive;
     bool attackOnCooldown;
     bool inRange;
@@ -16,10 +17,18 @@ public class EnemyAttack : MonoBehaviour
         DamageAreaActive = false;
         gameObject.SetActive(DamageAreaActive);
     } 
+=======
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+>>>>>>> 63f1b3eb (Adding Health and Attack Tags and things)
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
         if (inRange)
         {
             currentWindUpTime++;
@@ -42,5 +51,8 @@ public class EnemyAttack : MonoBehaviour
         DamageAreaActive = true;
         gameObject.SetActive(DamageAreaActive);
         DamageAreaActive = false;
+=======
+        
+>>>>>>> 63f1b3eb (Adding Health and Attack Tags and things)
     }
 }
